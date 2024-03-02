483617af866e7a1eb14e058781bb2c4a8fff9fa6
Mehakpreet417 <64402765+Mehakpreet417@users.noreply.github.com>
2024-03-02 19:06:47 +0530

    index.tsx

--- utilis/index.tsx
+++ utilis/index.tsx
@@ -16,27 +16,28 @@ interface Book {
 }
   
   export const fetchBooks = async (genre: string, searchQuery?: string): Promise<Book[]> => {
-    try {
-      let apiUrl = `http://gutendex.com/books/?topic=${genre}`;
-      if (searchQuery) {
-        apiUrl += `&search=${searchQuery}`;
-      }
-      const response = await fetch(apiUrl);
-      if (!response.ok) {
-        throw new Error('Failed to fetch books');
-      }
-      const data = await response.json();
-      const books: Book[] = data.results
-          .filter((book: any) => 'image/jpeg' in book.formats)
-          .map((book: any) => ({
+  try {
+    let apiUrl = `https://gutendex.com/books/?topic=${genre}`;
+    if (searchQuery) {
+      apiUrl += `&search=${searchQuery}`;
+    }
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error('Failed to fetch books');
+    }
+    const data = await response.json();
+    const books: Book[] = data.results
+      .filter((book: any) => 'image/jpeg' in book.formats)
+      .map((book: any) => ({
         id: book.id,
         title: book.title,
         authors: book.authors ?? [], 
         formats: book.formats ?? {}
       }));
-      return books;
-    } catch (error) {
-      throw new Error(`Error fetching books: ${(error as Error).message}`);
-    }
-  };
+    return books;
+  } catch (error) {
+    throw new Error(`Error fetching books: ${(error as Error).message}`);
+  }
+};
+
   
